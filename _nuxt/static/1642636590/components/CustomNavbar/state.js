4c9ce02028e7e58ff045748e36a60abe425bf492
taylandogan <taylandogan@users.noreply.github.com>
2022-01-19 23:58:13 +0000

    deploy: 59e18dd32276a74b99d536ca2dd7a7e2ac8e46e4

--- _nuxt/static/1642636590/components/CustomNavbar/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftaylandogan.github.io\u002F_nuxt\u002Fstatic\u002F1642636590",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcomponents\u002FCustomNavbar",config:{_app:{basePath:"\u002Ftaylandogan.github.io\u002F",assetsPath:"\u002Ftaylandogan.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));

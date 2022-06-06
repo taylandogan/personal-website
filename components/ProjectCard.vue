2fe7daab5d5c28516b694729e7a9e18b1c7a1857
Taylan Dogan <ttaylan.dogann@gmail.com>
2022-06-06 17:07:36 +0200

    Add ProjectCards to masonry wall

---
+++ components/ProjectCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="project-card">
+    <a :href="link">
+      <div class="project-image">
+        <img :src="imgSrc()"/>
+      </div>
+      <div>
+        <p class="project-name">{{ name }} <span v-if="id">#{{ id }}</span></p>
+        <p v-if="description" class="project-description">{{ description }}</p>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: [String, Number],
+      default: null
+    },
+    name: {
+      type: String,
+      default: '',
+      required: true
+    },
+    img: {
+      type: String,
+      default: '',
+      required: true
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    link: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    imgSrc() {
+      try {
+        return require(`~/assets/images/${this.img}`)
+      } catch (error) {
+        return null
+      }
+    }
+  }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'Lato';
+  src: local('Lato'),
+    url('~/assets/fonts/Lato/Lato-Regular.ttf') format('truetype');
+}
+
+.project-card {
+  background-color: #ffffff;
+  color: #8BACD9;
+  border-radius: 16px;
+  padding: 24px;
+  box-shadow: 0 25px 50px 0 rgba(0,0,0,0.1);
+}
+
+.project-image {
+    border-radius: 8px;
+    max-width: 100%;
+    max-height: 100%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.project-content {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    padding: 24px 0 16px 0;
+}
+
+.project-name {
+    color: #1f9102;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.project-description {
+    color: #000000;
+}
+
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -3,23 +3,69 @@
     <CustomNavbar></CustomNavbar>
     <div class="container">
       <br>
-      <hr>
       <br>
-      <ul class="grid grid-cols-3 gap-8 mt-8">
-        <ProjectPreview v-for="project in projects" :key="project.slug" :project="project"></ProjectPreview>
+       <ul class="grid grid-cols-3 gap-8 mt-8">
+        <hr/>
+        <vue-masonry-wall :items="items" :options="options" :ssr="{columns: 2}">
+          <template v-slot:default="{ item }">
+            <div :key="item.id" class="item">
+              <ProjectCard :id="item.id" :name="item.title" :img="item.img" :description="item.description" :link="item.link"/>
+            </div>
+          </template>
+        </vue-masonry-wall>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import VueMasonryWall from "vue-masonry-wall";
+import ProjectCard from '../components/ProjectCard';
+
+const items = []
+const options = {
+  width: 350,
+  padding: {
+    default: 10,
+    1: 4,
+    2: 6
+  },
+}
+
 export default {
-    async asyncData({ $content }) {
-        const projects = await $content('projects')
-            .sortBy('createdAt', 'desc')
-            .fetch();
-        return { projects }
-    },
+  components: {
+    VueMasonryWall,
+    ProjectCard
+  },
+
+  data() {
+    return {
+      items,
+      options,
+    };
+  },
+
+  mounted() {
+    this.fetchItems()
+  },
+
+  methods: {
+    async fetchItems() {
+      const projects = await this.$content('projects')
+        .sortBy('createdAt', 'desc')
+        .fetch();
+
+      for (let i = 0; i < projects.length; i++) {
+        this.items.push({
+          id: i,
+          title: projects[i].title,
+          img: projects[i].img,
+          description: projects[i].description,
+          link: `/project/${projects[i].slug}`
+        })
+      }
+    }
+  },
 }
 </script>
 

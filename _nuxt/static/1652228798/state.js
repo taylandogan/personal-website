deba021a7d4c07532b587952ee73d71abf5f94cf
taylandogan <taylandogan@users.noreply.github.com>
2022-06-06 15:08:53 +0000

    deploy: 2fe7daab5d5c28516b694729e7a9e18b1c7a1857

--- _nuxt/static/1652228798/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652228798",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3792b709"}}}}(null,"\u002F"));
